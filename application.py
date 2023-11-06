1308ff8526cf4ca62914302eca96bbc588da0bf5
AIM1rage <michael050100@gmail.com>
2023-11-06 14:57:39 +0500

    move application.py

---
+++ application.py
@@ -0,0 +1,5 @@
+import prompt_toolkit
+from src.http_client import HttpClient
+
+if __name__ == '__main__':
+    user = prompt_toolkit.prompt()
--- src/application.py
+++
@@ -1,5 +0,0 @@
-import prompt_toolkit
-from http_client import HttpClient
-
-if __name__ == '__main__':
-    ...
